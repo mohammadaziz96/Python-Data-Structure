a15d09c3fdc16286ce0c6aa5028014f1a78ed362
Mohammad Aziz <aziznitham@gmail.com>
2020-03-27 12:33:13 +0530

    Add files via upload

---
+++ reading_and_converting.py
@@ -0,0 +1,13 @@
+name=input("Enter")
+try:
+    print(name)
+except:
+    print("Enter an string")
+name1=input("Enter")
+try:
+    name2=int(name1)
+    name3=name2-1
+    print(name3)
+except:
+    print("Enter a integer number")
+
